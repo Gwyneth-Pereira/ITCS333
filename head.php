911343adba4fa8cd977e73bf3f4e5467db21ee54
Ibrahim Kubaisy <61657294+ibrahimkubaisy@users.noreply.github.com>
2020-12-06 00:08:51 +0300

    bug fixing
    paths fixings
    
    plus adding notAuthorized.php template to add to other pages directly (can design it more later and add more stuff in it)

--- head.php
+++ head.php
@@ -1,16 +1,12 @@
 	<!-- FAVICON  -->
-    <link rel="shortcut icon" href="../images/favicon.png">
+    <link rel="shortcut icon" href="images/favicon.png">
 
     <!-- CSS -->
-	<link rel="stylesheet" type="text/css" href="../css/aos.css">
-	<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="../css/bootstrap-grid.css">
-	<link rel="stylesheet" type="text/css" href="../css/bootstrap-reboot.css">
-	<link rel="stylesheet" type="text/css" href="../css/custom.css">
+	<link rel="stylesheet" type="text/css" href="css/aos.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-grid.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-reboot.css">
+	<link rel="stylesheet" type="text/css" href="css/custom.css">
 
 	<!-- GOOGLE FONT -->
-    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,500,600,700' rel='stylesheet' type='text/css'>
-
-    <!-- Cross-Platform Smooth Scrolling Script -->
-    <script type="text/javascript" src="../js/jquery-3.5.1.js"></script>
-    <script type="text/javascript" src="../js/scroll.js"></script>
+    <link rel='stylesheet' type='text/css' href='http://fonts.googleapis.com/css?family=Montserrat:400,500,600,700'>
--- signup.php
+++ signup.php
@@ -1,27 +1,38 @@
 <?php
+session_start();
+require('connection.php');
+extract($_POST);
 try{
-    require('connection.php');
-    //simple validation
-    if (trim($name)=='' ||  trim($username)=='' || trim($password)=='' || trim($cpassword)=='' || trim($DOB)=='')
-      echo 'Missing Information';
+    // simple validation, BUT REGEX IS REQUIRED!!
+    if (trim($name)=='' ||  trim($email)=='' ||  trim($username)=='' || trim($password)=='' || trim($cpassword)=='' || trim($DOB)==''){
+        echo '<p>You Are Missing Information Please <a href="register.php">Try Again</a></p>';
+    }
     else if ($password!=$cpassword)
-      echo "Passwords don't match";
+        echo "Passwords don't match";
     else {
-      $hashed = password_hash($password, PASSWORD_DEFAULT); //this is better function than md5 (more secure)
-      // $dob="$y-$m-$d"; maybe this is needed to be inserted in the database
-      $sql="insert into users values(NULL, '$name', '$username', '$hashed','$DOB','customer')";
-      $r=$db->exec($sql);
-      $db=null;
+        $hashed = password_hash($password, PASSWORD_DEFAULT); // this is better function than md5 (more secure)
+        // $dob="$y-$m-$d"; // maybe this is needed to be inserted in the database
+        $sql = $db->prepare("INSERT INTO users VALUES(NULL, :username, :hashed, :name, :email);");
+        
+        $sql->bindParam(':username', $username);
+        $sql->bindParam(':hashed', $hashed);
+        $sql->bindParam(':name', $name);
+        $sql->bindParam(':email', $email);
+        // $sql->bindParam(':DOB', $DOB);
 
-      if ($r==1)
-        die('Successful registeration');
-      else {
-        echo 'Failed, please try again later';
+        $result = $sql->execute();
+        $db = null;
 
-      }
+        if ($result==1)
+            header('location: index.php');
+        else
+            echo '<p>Something wrong happened... Please <a href="register.php">Try Again</a></p>';
+        
+        $_SESSION['active'] = true;
+        $_SESSION['username'] = $username;
     }
 }
-catch(PDOException $ex) { //ex is the error object
-    die ("Error Message ".$ex->getMessage());
+catch(PDOException $ex) {
+    die("Error Message ".$ex->getMessage());
 }
 ?>
