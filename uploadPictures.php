38224461fc933633ea5aa267e8806d9908143a77
Ibrahim Kubaisy <61657294+ibrahimkubaisy@users.noreply.github.com>
2020-12-12 04:59:56 +0300

    minor updates

---
+++ uploadPictures.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+if (!isset($_SESSION['active'])) {
+	header('location: notAuthorized.php');
+}
+// require('connection.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Upload Product Pictures</title>
+	<?php include 'head.php'; ?>
+</head>
+<body>
+	<?php include 'header.php'; ?>
+
+	<div class="container">
+		<h1>Create your auction</h1>
+	</div>
+
+	<form method="POST" class="form-group w-25 mx-auto text-center">
+        
+    </form>
+
+	<?php include 'scripts.php'; ?>
+</body>
+</html>
