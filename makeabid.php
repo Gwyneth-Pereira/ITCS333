136ed1cc879e2e98617f9e630cc329a175af8d6c
Ibrahim Kubaisy <61657294+ibrahimkubaisy@users.noreply.github.com>
2020-12-12 05:00:07 +0300

    minor updates

--- makeabid.php
+++ makeabid.php
@@ -12,28 +12,28 @@ if (preg_match($bidpattern,$userbid)) {
 
 		require('connection.php');
 		
-$sql="SELECT * FROM 'auctions' WHERE product=$pid";
-$r=$db->query($sql); 
-$prod=$r->fetch(PDO::FETCH_ASSOC);
-if($prod['bid']<=$userbid){
-	$bidPreparedStatement=$db->prepare("UPDATE 'auctions' SET bid=? WHERE product=?");
-	$bidPreparedStatement->blindParam(1,$userbid);
-	$bidPreparedStatement->blindParam(2,$pid);
-	$bidPreparedStatement->execute();
+	$sql="SELECT * FROM 'auctions' WHERE product=$pid";
+	$r=$db->query($sql); 
+	$prod=$r->fetch(PDO::FETCH_ASSOC);
+	if($prod['bid']<=$userbid){
+		$bidPreparedStatement=$db->prepare("UPDATE 'auctions' SET bid=? WHERE product=?");
+		$bidPreparedStatement->blindParam(1,$userbid);
+		$bidPreparedStatement->blindParam(2,$pid);
+		$bidPreparedStatement->execute();
 
-}
-
-else 
-	echo "Your bid is lower than the existing bid";
-
-
-		$db=null;
 	}
 
-	catch(PDOExecption $e){
-	die("There is an Error: ".$e->getMessage());
-		} 
-}
+	else 
+		echo "Your bid is lower than the existing bid";
+
+
+			$db=null;
+		}
+
+		catch(PDOExecption $e){
+		die("There is an Error: ".$e->getMessage());
+			}
+	}
 }
 
 
@@ -45,7 +45,7 @@ else
 <body>
 <form method="post">
 <label for="bid">Your Bid: </label><input type="text" name="userbid">
-<!---<input type='hidden' name='pid' value=pid>--->	
+<input type='hidden' name='pid' value='pid'>	
 <input type="submit" name="makebid" value="Make Bid">
 </form>
 </body>
