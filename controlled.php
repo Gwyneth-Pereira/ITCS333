a3e3ca82b29214ac9447c16b97bd780881d62fc5
reemerz <67433330+reemerz@users.noreply.github.com>
2020-12-15 02:27:44 +0300

    controlled file

---
+++ controlled.php
@@ -0,0 +1,22 @@
+<?php 
+date_default_timezone_set("Asia/Bahrain");
+try{
+    require('connection.php');
+    $sql="SELECT id, end FROM auctions WHERE status='active'";
+    $rs=$db->query($sql);
+
+    foreach($rs as $row){
+        $endttime=UNIX_TIMESTAMP($row['end']);
+        $current=UNIX_TIMESTAMP(date('Y-m-d H:i:s'));
+        $auctionid = $row['id'];
+        if($current>$endttime){
+            $newsql="UPDATE auctions SET status='not active' WHERE id=$auctionid";
+            $update=$db->exec($newsql);
+        }
+    }
+    $db=null;
+} 
+catch (PDOException $e){
+    die($e->getMessage());
+}
+?>
