a03da25dd3a33a528cf6da704a866c9055ce5fdd
Ibrahim Kubaisy <61657294+ibrahimkubaisy@users.noreply.github.com>
2020-12-16 02:15:36 +0300

    updates

--- createAuction.php
+++ createAuction.php
@@ -5,6 +5,33 @@ if (!isset($_SESSION['active'])) {
 	exit;
 }
 // require('connection.php');
+
+extract($_POST);
+if (isset($submit)) {
+	try {
+		require('connection.php');
+		
+		$db->beginTransaction();
+		
+		$sql = $db->prepare("INSERT INTO products VALUES(NULL, ?, ?, ?)");
+		$sql->execute(array($name, $details, $category));
+		
+		$productid = $db->lastInsertId();
+		
+		$sql = $db->prepare("INSERT INTO auctions VALUES(NULL, ?, ?, NOW(), ?, ?, NULL, NULL, ?)");
+		$sql->execute(array($_SESSION['username'], $productid, $end, $price, 'active'));
+		
+		$db->commit();
+		// $db=null;
+	} catch (PDOException $ex) {
+		$db->rollBack();
+		echo $ex->getMessage();
+		exit;
+	}
+
+	header("location: uploadPictures.php?productid=$productid");
+	exit;
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -15,9 +42,9 @@ if (!isset($_SESSION['active'])) {
 </head>
 <body>
 	<?php include 'header.php'; ?>
-
+	
 	<form method="POST" class="form-group w-25 mx-auto text-center">
-        <h1 class="font-weight-bold mb-4">Create Auction</h1>
+		<h1 class="font-weight-bold mb-4">Create Auction</h1>
         
 		<p class="h5 text-left font-weight-bold mt-4">Product name:</p>
         <p><input class="form-control" type="text" name="name" placeholder="Product Name" required/></p>
@@ -25,7 +52,7 @@ if (!isset($_SESSION['active'])) {
 		<p class="h5 text-left font-weight-bold mt-4">Cateogry:</p>
         <p>
         	<select class="form-control" type='text' name='category' placeholder="Category">
-        	<option>Art</option>
+				<option>Art</option>
         	<option>Books</option>
         	<option>Clothings</option>
         	<option>Electronics</option>
@@ -42,47 +69,19 @@ if (!isset($_SESSION['active'])) {
 		<p class="h5 text-left font-weight-bold mt-4">Start Price:</p>
         <div class="input-group mb-3">
 			<div class="input-group-prepend">
-    			<span class="input-group-text">$</span>
-  			</div>
+				<span class="input-group-text">BD</span>
+			</div>
 			<input class="form-control" type="number" step="any" name="price" placeholder="Price" required/>
 		</div>
         
 		<p class="h5 text-left font-weight-bold mt-4">End Time/Date:</p>
         <p><input class="form-control" type="datetime-local" id="end" name="end" step="1" value="<?php echo date('Y-m-d H:i:s'); ?>" min="<?php echo date('Y-m-d').'T'.date('H:i:s'); ?>"
-         max="<?php echo (date('Y')+1).date('-m-d').'T'.date('H:i:s'); ?>" required></p>
+		max="<?php echo (date('Y')+1).date('-m-d').'T'.date('H:i:s'); ?>" required></p>
         
         <p><input class="btn btn-danger" type="submit" name="submit" value="Create Auction"></p>
     </form>
-
+	
 	<?php include 'scripts.php'; ?>
-
-<?php
-extract($_POST);
-
-if (isset($submit)) {
-	try {
-		require('connection.php');
-		
-		$db->beginTransaction();
-		
-		$sql = $db->prepare("INSERT INTO products VALUES(NULL, ?, ?, ?, NULL)");
-		$sql->execute(array($name, $details, $category));
-		
-		$productid = $db->lastInsertId();
-		
-		$sql = $db->prepare("INSERT INTO auctions VALUES(NULL, ?, ?, NOW(), ?, ?, ?)");
-		$sql->execute(array($_SESSION['username'], $productid, $end, $price, 'active'));
-		
-		$db->commit();
-		// $db=null;
-	} catch (PDOException $ex) {
-		$db->rollBack();
-		echo $ex->getMessage();
-		exit;
-	}
-
-	header('location: uploadPictures.php?productid=$productid');
-}
-?>
+	
 </body>
 </html>
