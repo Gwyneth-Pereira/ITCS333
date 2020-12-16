5af3ae12dcfbf2466a6ab93e5526490dc4c34ebc
reemerz <67433330+reemerz@users.noreply.github.com>
2020-12-16 03:12:11 +0300

    completeTransaction.php

--- controlled.php
+++ controlled.php
@@ -1,18 +1,22 @@
 <?php 
-// active -> pending -> successful -> completed/failed
+// AUCTION STATUSES: active -> pending/no participation -> successful -> completed/failed
 date_default_timezone_set("Asia/Bahrain");
 try{
     require('connection.php');
-    $sql="SELECT id, end FROM auctions WHERE status='active'";
+    $sql="SELECT id, end, bidder FROM auctions WHERE status='active'";
     $rs=$db->query($sql);
 
     foreach($rs as $row){
         $endttime=strtotime($row['end']);
-        // $current=UNIX_TIMESTAMP(date('Y-m-d H:i:s'));
         $current=time();
         $auctionid = $row['id'];
         if($current>$endttime){
-            $newsql="UPDATE auctions SET status='pending' WHERE id=$auctionid";
+            if ($row['bidder']==NULL){
+                $newsql="UPDATE auctions SET status='noparticipation' WHERE id=$auctionid";
+            } 
+            else{
+                $newsql="UPDATE auctions SET status='pending' WHERE id=$auctionid";
+            }
             $update=$db->exec($newsql);
         }
     }
