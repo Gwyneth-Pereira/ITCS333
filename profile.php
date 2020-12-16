72db616153b99f747adc1c35d0466afcb4f290cf
Ibrahim Kubaisy <61657294+ibrahimkubaisy@users.noreply.github.com>
2020-12-17 01:31:11 +0300

    Update profile.php

--- profile.php
+++ profile.php
@@ -93,7 +93,7 @@ require('controlled.php');
 					echo "</tr>";
 					$username = $_SESSION['username'];
 					$sql = $db->prepare("SELECT * FROM users WHERE username=?");
-					if ($sql->execute(array('admin'))){
+					if ($sql->execute(array($_SESSION['username']))){
 						while ($info = $sql->fetch(PDO::FETCH_ASSOC)) {   
 							echo "<tr>";
 							echo "<td>".$info['name']."</td>";
