13a59f3d55d25c1ea5f7501ffcd4e446fe0b3a9a
reemerz <67433330+reemerz@users.noreply.github.com>
2020-12-17 23:27:58 +0300

    unique

--- completeTransaction.php
+++ completeTransaction.php
@@ -101,100 +101,95 @@ extract($_REQUEST);
 </head>
 <body>
     <?php include 'header.php'; ?>
-    
-    
     <?php
-if(isset($submit)){
-    $namepattern="/^[a-zA-Z]{3,}(?:\s[a-zA-Z]{3,})+$/";
-    $numberpattern="/^((\+|00)973)?\s?\d{8}$/";
-    $emailpattern="/^[a-zA-Z0-9._-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,5}$/";
-    $addresspattern="/^(.|\s)*[a-zA-Z]{3,200}(.|\s)*$/";
-	if ($JSEnabled=="FALSE"){
-        if (trim($name)=='' || trim($number)=='' || trim($email)=='' || trim($address)==''){
-            header('location:completeTransaction.php?error=missing');
-			exit;
-		}
-		elseif(!preg_match($namepattern,$name)){
-            header('location:completeTransaction.php?error=wrongname');
-			exit;
-		}
-		elseif(!preg_match($numberpattern,$number)){
-            header('location:completeTransaction.php?error=wrongnumber');
-			exit;
+    if(isset($submit)){
+        $namepattern="/^[a-zA-Z]{3,}(?:\s[a-zA-Z]{3,})+$/";
+        $numberpattern="/^((\+|00)973)?\s?\d{8}$/";
+        $emailpattern="/^[a-zA-Z0-9._-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,5}$/";
+        $addresspattern="/^(.|\s)*[a-zA-Z]{3,200}(.|\s)*$/";
+        if ($JSEnabled=="FALSE"){
+            if (trim($name)=='' || trim($number)=='' || trim($email)=='' || trim($address)==''){
+                header('location:completeTransaction.php?error=missing');
+                exit;
+            }
+            elseif(!preg_match($namepattern,$name)){
+                header('location:completeTransaction.php?error=wrongname');
+                exit;
+            }
+            elseif(!preg_match($numberpattern,$number)){
+                header('location:completeTransaction.php?error=wrongnumber');
+                exit;
+            }
+            elseif(!preg_match($emailpattern,$email)){
+                header('location:completeTransaction.php?error=wrongemail');
+                exit;
+            }
+            elseif(!preg_match($addresspattern,$address)){
+                header('location:completeTransaction.php?error=wrongaddress');
+                exit;
+            }
         }
-        elseif(!preg_match($emailpattern,$email)){
-            header('location:completeTransaction.php?error=wrongemail');
-			exit;
+        
+        try{
+            require('connection.php');
+            
+            $db->beginTransaction();
+            
+            $sql=$db->prepare("INSERT INTO transactions VALUES(NULL, ?, ?, ?, ?, ?)");
+            $sql->execute(array($name, $auctionid, $number, $email, $address));
+            
+            
+            $sql=$db->prepare("UPDATE auctions SET status=? WHERE id=?");
+            $sql->execute(array('successful', $auctionid));
+            
+            $db->commit();
         }
-        elseif(!preg_match($addresspattern,$address)){
-			header('location:completeTransaction.php?error=wrongaddress');
-			exit;
-		}
+        catch (PDOException $e){
+            $db->rollBack();
+            echo $ex->getMessage();
+        }
+        
+        header('location: myAuctions.php');
+        exit;    
     }
-    
-    try{
-        require('connection.php');
-        
-        $db->beginTransaction();
-        
-        $sql=$db->prepare("INSERT INTO transactions VALUES(NULL, ?, ?, ?, ?, ?)");
-        $sql->execute(array($name, $auctionid, $number, $email, $address));
-        
-        
-        $sql=$db->prepare("UPDATE auctions SET status=? WHERE id=?");
-        $sql->execute(array('successful', $auctionid));
-        
-        $db->commit();
-    }
-    catch (PDOException $e){
-        $db->rollBack();
-        echo $ex->getMessage();
-    }
-    
-    header('location: myAuctions.php');
-    exit;    
-}
 
-if(isset($auctionid)){
-    ?>
-    <?php
-    $msg="";
-	if(isset($error)&& $error=='missing'){
-        $msg="Missing Information";
-	}
-	elseif(isset($error)&& $error=='wrongname'){
-        $msg="Please enter your first and last name seperated by a space";
-	}
-	elseif(isset($error)&& $error=='wrongnumber'){
-        $msg="Invalid Number";
+    if(isset($auctionid)){
+        $msg="";
+        if(isset($error)&& $error=='missing'){
+            $msg="Missing Information";
+        }
+        elseif(isset($error)&& $error=='wrongname'){
+            $msg="Please enter your first and last name seperated by a space";
+        }
+        elseif(isset($error)&& $error=='wrongnumber'){
+            $msg="Invalid Number";
+        }
+        elseif(isset($error)&& $error=='wrongemail'){
+            $msg="Invalid Email: must be in the format ****@****.***";
+        }
+        elseif(isset($error)&& $error=='wrongaddress'){
+            $msg="Invalid Address";
+        }
+        ?>
+        <form method="POST" class="form-group w-25 mx-auto text-center" onSubmit="return checkUserInputs();">
+            <h1 class="font-weight-bold mb-4">Complete Transaction</h1>
+            <!-- <p>Full Name:</p> -->
+            <p><input class="form-control" required type="text" name="name" placeholder="Full Name" onkeyup="checkName(this.value)"/><span id='nmsg'></span></p>
+            <!-- <p>Contact Number:</p> -->
+            <p><input class="form-control" required type="text" name="number" placeholder="Contact Number"onkeyup="checkNumber(this.value)"/><span id='nummsg'></span></p>
+            <!-- <p>Email:</p> -->
+            <p><input class="form-control" required type="email" name="email" placeholder="Email" onkeyup="checkEmail(this.value)"/><span id='emsg'></span></p>
+            <!-- <p>Address:</p> -->
+            <p><textarea class="form-control" required name="address" cols="30" rows="10" placeholder="Delivery Address" onkeyup="checkAddress(this.value)"></textarea><span id='amsg'></span></p>
+            <!-- <p>Hidden Auction ID:</p> -->
+            <p><input  type="hidden" name="auctionid" value="$auctionid"/></p>
+            <input type='hidden' name='JSEnabled' value='FALSE'/>
+            <h3 class='text-danger mb-4 mt-4'><?php echo $msg;?></h3>
+            <p><input class="btn btn-danger" type="submit" name="submit" value="Complete"></p>
+        </form>
+        <?php    
     }
-    elseif(isset($error)&& $error=='wrongemail'){
-        $msg="Invalid Email: must be in the format ****@****.***";
-    }
-    elseif(isset($error)&& $error=='wrongaddress'){
-        $msg="Invalid Address";
-    }
-    ?>
-    <form method="POST" class="form-group w-25 mx-auto text-center" onSubmit="return checkUserInputs();">
-        <h1 class="font-weight-bold mb-4">Complete Transaction</h1>
-        <!-- <p>Full Name:</p> -->
-        <p><input class="form-control" required type="text" name="name" placeholder="Full Name" onkeyup="checkName(this.value)"/><span id='nmsg'></span></p>
-        <!-- <p>Contact Number:</p> -->
-        <p><input class="form-control" required type="text" name="number" placeholder="Contact Number"onkeyup="checkNumber(this.value)"/><span id='nummsg'></span></p>
-        <!-- <p>Email:</p> -->
-        <p><input class="form-control" required type="email" name="email" placeholder="Email" onkeyup="checkEmail(this.value)"/><span id='emsg'></span></p>
-        <!-- <p>Address:</p> -->
-        <p><textarea class="form-control" required name="address" cols="30" rows="10" placeholder="Delivery Address" onkeyup="checkAddress(this.value)"></textarea><span id='amsg'></span></p>
-        <!-- <p>Hidden Auction ID:</p> -->
-        <p><input  type="hidden" name="auctionid" value="$auctionid"/></p>
-        <input type='hidden' name='JSEnabled' value='FALSE'/>
-        <h3 class='text-danger mb-4 mt-4'><?php echo $msg;?></h3>
-        <p><input class="btn btn-danger" type="submit" name="submit" value="Complete"></p>
-    </form>
-    <?php    
-}
 ?>
-    
     <?php include 'scripts.php'; ?>
 </body>
 </html>
--- signup.php
+++ signup.php
@@ -38,25 +38,43 @@ try{
 	  }
 	}
 	else{
-		$hashed = sha1($password); // this is better function than md5 and password_hash (more secure)
-		$sql = $db->prepare("INSERT INTO users VALUES(NULL, :username, :hashed, :name, :email);");
 
-		$sql->bindParam(':username', $username);
-		$sql->bindParam(':hashed', $hashed);
-		$sql->bindParam(':name', $name);
-		$sql->bindParam(':email', $email);
-		
+		$usersql="SELECT * FROM users WHERE username='$username'";
+		$emailsql="SELECT * FROM users WHERE email='$email'";
+		$user=$db->query($usersql);
+		$email=$db->query($emailsql);
 
-		$result = $sql->execute();
-		$db = null;
-
-		if ($result==1){
-			$_SESSION['active'] = true;
-			$_SESSION['username'] = $username;
-			header('location: index.php');
+		if($user->rowCount() > 0){ //username exists
+			header('location:register.php?error=userexist');
+			exit;
 		}
+
+		elseif($email->rowCount() > 0){ //email exists
+			header('location:register.php?error=emailexist');
+			exit;
+		}
+		
 		else{
-			echo '<p>Something wrong happened... Please <a href="register.php">Try Again</a></p>';
+			$hashed = sha1($password); // this is better function than md5 and password_hash (more secure)
+			$sql = $db->prepare("INSERT INTO users VALUES(NULL, :username, :hashed, :name, :email);");
+
+			$sql->bindParam(':username', $username);
+			$sql->bindParam(':hashed', $hashed);
+			$sql->bindParam(':name', $name);
+			$sql->bindParam(':email', $email);
+			
+
+			$result = $sql->execute();
+			$db = null;
+
+			if ($result==1){
+				$_SESSION['active'] = true;
+				$_SESSION['username'] = $username;
+				header('location: index.php');
+			}
+			else{
+				echo '<p>Something wrong happened... Please <a href="register.php">Try Again</a></p>';
+			}
 		}
 
 	}
