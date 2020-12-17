cc08dd0b498d997353651a9368cbe4ca1b118a72
20188447 <73710059+20188447@users.noreply.github.com>
2020-12-18 01:51:29 +0300

    Add files via upload
    without scroll and login authorization

---
+++ chat.php
@@ -0,0 +1,65 @@
+<html>
+<head>
+<title>Private Communication </title>
+<link  rel="stylesheet" href="chatstyle.css" />
+<script>
+
+</script>
+</head>
+<body>
+<?php
+session_start();
+extract($_REQUEST);
+date_default_timezone_set("Asia/Bahrain");
+$u1= $_SESSION['username'];
+// $t=date("D,d-n-y,h:i:s A");
+$auctionid=6;
+	try 
+	{	
+		require("connection.php");
+		$db->beginTransaction();
+		
+		if(isset($submitmsg)=="Send")
+		{
+			$smt=$db->prepare("INSERT INTO chats Values (NULL,:auction,:name, :msg, NOW())");
+			$smt->bindParam(':auction',$auctionid);
+			$smt->bindParam(':name',$u1);
+			$smt->bindParam(':msg',$usermsg);
+			$smt->execute();
+		}
+		$row=$db->prepare("SELECT * FROM chats Where auction=:aid");
+		$row->bindParam(':aid',$auctionid);
+		$row->execute();
+		
+		$db->commit();
+	}
+	catch (PDOException $ex) 
+	{
+		$db->rollBack();
+		echo "ERROR:".$ex->getMessage();
+	}
+?>
+<div id="wrapper">
+    <div id="menu">
+        <p class="welcome">Welcome, <b> <?php echo $_SESSION['username']; ?></b></p>
+        <p class="logout"><a id="exit" href="myAuctions.php">Exit Chat</a></p>
+        <div style="clear:both"></div>
+    </div>
+	<div id="chatbox">
+	<?php
+	
+	while($r=$row->fetch())
+			{
+				echo "<p><b>".$r['username']." </b></p>";
+				echo "<p style='font-size:20px'>".$r['message']."</p>";
+				echo "<p style='font-size:10px'>".$r['time']."</p></br>";
+			}
+	?>
+	</div>
+	<form method="POST">
+	     <input name="usermsg" id="usermsg" type="text" />
+        <input type="submit" name="submitmsg" id="submit" value="Send" />
+    </form>	
+</div>  
+</body>
+</html>
---
+++ chats.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 17, 2020 at 02:52 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `chat_app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chats`
+--
+
+CREATE TABLE `chats` (
+  `id` int(11) NOT NULL,
+  `auction` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `message` varchar(200) NOT NULL,
+  `time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `chats`
+--
+
+INSERT INTO `chats` (`id`, `auction`, `username`, `message`, `time`) VALUES
+(1, 6, 'admin', 'heyy', '2020-12-17 04:50:59');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chats`
+--
+ALTER TABLE `chats`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `chats`
+--
+ALTER TABLE `chats`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
