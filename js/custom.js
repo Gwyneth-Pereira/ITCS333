0965b258c6708bb5547b602f29f09cdf4452313f
Ibrahim Kubaisy <61657294+ibrahimkubaisy@users.noreply.github.com>
2020-12-20 20:00:30 +0300

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,73 +1,98 @@
 //SCROLL TOP ON PAGE REFRESH
 $(window).on('beforeunload', function() {
-    $(window).scrollTop(0);
+  $(window).scrollTop(0);
 });
 
-// $(function () {
+$(function () {
 
-// 	// AOS ANIMATION
-// 	AOS.init({
-// 	  disable: 'mobile',
-// 	  duration: 800,
-// 	  anchorPlacement: 'center-bottom'
-// 	});
+  // AOS ANIMATION
+  AOS.init({
+    disable: 'mobile',
+    duration: 800,
+    anchorPlacement: 'center-bottom'
+  });
 
 
-// 	// SMOOTHSCROLL NAVBAR
-// 	$(function() {
-// 	  $('.navbar a, .hero-text a').on('click', function(event) {
-// 	    var $anchor = $(this);
-// 	    $('html, body').stop().animate({
-// 	        scrollTop: $($anchor.attr('href')).offset().top - 49
-// 	    }, 1000);
-// 	    event.preventDefault();
-// 	  });
-// 	});    
-// });
+  // SMOOTHSCROLL NAVBAR
+  $(function() {
+    $('.navbar a, .hero-text a').on('click', function(event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+          scrollTop: $($anchor.attr('href')).offset().top - 49
+      }, 1000);
+      event.preventDefault();
+    });
+  });    
+});
 
 // Search Bar AJAX
 var xmlHttp;
 function GetXmlHttpObject() {
-  xmlHttp=null;
+xmlHttp=null;
+try
+  {
+  // Firefox, Opera 8.0+, Safari
+  xmlHttp=new XMLHttpRequest();
+  }
+catch (e)
+  {
+  // Internet Explorer 
   try
-    {
-    // Firefox, Opera 8.0+, Safari
-    xmlHttp=new XMLHttpRequest();
-    }
+    { xmlHttp=new ActiveXObject("Msxml2.XMLHTTP"); }
   catch (e)
-    {
-    // Internet Explorer 
-    try
-      { xmlHttp=new ActiveXObject("Msxml2.XMLHTTP"); }
-    catch (e)
-      { xmlHttp=new ActiveXObject("Microsoft.XMLHTTP"); }
-    }
-  return xmlHttp;
+    { xmlHttp=new ActiveXObject("Microsoft.XMLHTTP"); }
+  }
+return xmlHttp;
 }
 
 function searchText(productsSearch){
-  xmlHttp=GetXmlHttpObject();
-  if (xmlHttp==null)  {
-    alert ("Your browser does not support AJAX! Please try another one!");
-    return;
-  }
+xmlHttp=GetXmlHttpObject();
+if (xmlHttp==null)  {
+  alert ("Your browser does not support AJAX! Please try another one!");
+  return;
+}
 
-	if (productsSearch.length==0){ //empty search
-    document.getElementById("searchResult").innerHTML="";
-    return;}
+if (productsSearch.length==0){ //empty search
+  document.getElementById("searchResult").innerHTML="";
+  return;}
 
-    document.getElementById("searchResult").innerHTML="hi";
-	
-	var url="search.php";
-  	url=url+"?searchfield="+productsSearch;
-  	url=url+"&searchID="+Math.random(); //for caching
-  xmlHttp.onreadystatechange=stateChanged;
-  xmlHttp.open("GET",url,true);
-  xmlHttp.send(null);
+var url="search.php";
+  url=url+"?searchfield="+productsSearch;
+  url=url+"&searchID="+Math.random(); //for caching
+xmlHttp.onreadystatechange=stateChanged;
+xmlHttp.open("GET",url,true);
+xmlHttp.send(null);
 }
 
 function stateChanged() {
-  if (xmlHttp.readyState==4){
-  	document.getElementById("searchResult").innerHTML=xmlHttp.responseText;
-  	}
+if (xmlHttp.readyState==4){
+  document.getElementById("searchResult").innerHTML=xmlHttp.responseText;
+  }
 }
+
+
+// smooth scrolling
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
